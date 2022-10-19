295dab6d8aa3202e01556741ffab1e78fe1c0162
AP Java Period 5 <apjavaperiod5@AC1MWK175JC9H.local>
2022-10-19 12:04:56 -0400

    bulk add

---
+++ 01-favorite-movie/movie.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class movie here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class movie
+{
+    public static void main(String[] args) {
+        System.out.println("whats your favorate movie? "); 
+        
+
+}
+}//end class
